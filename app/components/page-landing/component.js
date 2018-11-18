906802347f4ef4ff24b64e925461db6854ad29ef
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-11-18 17:19:37 +1100

    Make landing page create and poll the session user

--- app/components/page-landing/component.js
+++ app/components/page-landing/component.js
@@ -1,6 +1,37 @@
 import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   tagName: 'main',
-  elementId: 'page-landing'
+  elementId: 'page-landing',
+
+  store: service(),
+
+  // The session user we create on init()
+  user: undefined,
+
+  init() {
+    this._super(...arguments);
+    this.createSessionUser.perform();
+  },
+
+  createSessionUser: task(function*() {
+    const user = this.store.createRecord('sessionUser', {});
+    yield user.save();
+    this.set('user', user);
+    this.startUpdateLocationLoop.perform();
+  }),
+
+  startUpdateLocationLoop: task(function*() {
+    const fiveSeconds = 5 * 1000;
+    while (true) {
+      this.user.setProperties({
+        lat: 'xxx',
+        long: 'yyy'
+      });
+      yield this.user.save();
+      yield timeout(fiveSeconds);
+    }
+  }).restartable()
 });
